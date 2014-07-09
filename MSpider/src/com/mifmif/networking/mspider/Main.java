7c7767b5ec3d4cbe462ee35c3cb63939265d34fd
y.mifrah <y.mifrah@bluage.com>
2014-07-09 17:25:08 +0000

    Add header.

--- MSpider/src/com/mifmif/networking/mspider/Main.java
+++ MSpider/src/com/mifmif/networking/mspider/Main.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright 2014 y.mifrah
+ *
+
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.mifmif.networking.mspider;
 
 import java.io.IOException;
@@ -26,10 +43,10 @@ public class Main {
 	private String TAG_CSS_SELECTOR = "html body.question-page div.container div#content div div#mainbar div#question.question table tbody tr td.postcell div div.post-taglist";
 
 	public static void main(String[] args) throws IOException {
-		System.setProperty("java.net.useSystemProxies", "true");
-		// Website website = new Main().bootstrap();
-		// Engine engine = new Engine(website);
-		Engine engine = new Engine("http://www.stackoverflow.com");
+		// System.setProperty("java.net.useSystemProxies", "true");
+		Website website = new Main().bootstrap();
+		Engine engine = new Engine(website);
+		// Engine engine = new Engine("http://www.stackoverflow.com");
 		engine.start();
 	}
 
--- MSpider/src/com/mifmif/networking/mspider/database/dao/impl/JpaFieldDao.java
+++ MSpider/src/com/mifmif/networking/mspider/database/dao/impl/JpaFieldDao.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2014 y.mifrah
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.mifmif.networking.mspider.database.dao.impl;
 
 import java.util.List;
@@ -15,17 +31,13 @@ public class JpaFieldDao extends JpaDao<Long, Field> implements FieldDao {
 
 	@Override
 	public List<Field> findAllByUrlPattern(URLPattern pattern) {
-		List<Field> found = entityManager
-				.createNamedQuery("Field.findAllByUrlPattern", Field.class)
-				.setParameter("pattern", pattern).getResultList();
+		List<Field> found = entityManager.createNamedQuery("Field.findAllByUrlPattern", Field.class).setParameter("pattern", pattern).getResultList();
 		return found;
 	}
 
 	@Override
 	public Field findByUrlPatternAndName(URLPattern pattern, String fieldName) {
-		List<Field> found = entityManager
-				.createNamedQuery("Field.findAllByUrlPattern", Field.class)
-				.setParameter("pattern", pattern).getResultList();
+		List<Field> found = entityManager.createNamedQuery("Field.findAllByUrlPattern", Field.class).setParameter("pattern", pattern).getResultList();
 		return found.isEmpty() ? null : found.get(0);
 	}
 
